eae25899090033f73447d33b98eb4a0b5fbfe295
Kevin <35283725+kpeter51@users.noreply.github.com>
2019-01-17 21:17:20 -0800

    fixed file organization

--- Classwork/Classwork 1/SayHello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc, char *argv[]) {
-  printf("What is your name?\n");
-  char input[25];
-  char *name;
-  name = gets(input);
-  printf("Hello, %s\n", name);
-}
--- Classwork/Classwork01/sayhello.c
+++ Classwork/Classwork01/sayhello.c
@@ -1,2 +1,9 @@
 #include <stdio.h>
-int main(int argc, char *argv[]) { printf("Hello world"); }
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+  printf("What is your name?\n");
+  char input[25];
+  char *name;
+  name = gets(input);
+  printf("Hello, %s\n", name);
+}
