da4671bffc14f878fb37b41ad0db1af4ed4554c7
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2019-01-31 22:02:10 -0800

    sum working

--- Classwork/Classwork03/goFish.c
+++ Classwork/Classwork03/goFish.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-  while (1) {
-    int i = 0;
-    char temp[25];
-    char* number = gets(temp);
-    int i_value = atoi(number);
-    // printf("number is %d \n", i_value);
-    int my_numbs[25];
-    if (i_value == -9999) {
-      break;
+int sum(int num_array[25], int size) {
+    int total = 0;
+    for (int i = 0; i < size; i++) {
+        total = total + num_array[i];
     }
-    my_numbs[i] = i_value;
-  }
-  printf("Sum is %d", sum(my_numbs))
+    return total;
 }
 
-int sum(int num_array[]) {
-  int total = 0;
-  for (int i = 0; i < len(num_array); i++) {
-    total = total + num_array[i];
-  }
-  return total;
+int main() {
+    int my_numbs[25];
+    int i = 0;
+
+    while (1) {
+        char input[25];
+        char *number = gets(input);
+        int i_value = atoi(number);
+        printf("number is %d \n", i_value);
+        if (i_value == -9999) { break; }
+        my_numbs[i] = i_value;
+        i++;
+    }
+
+    printf("Sum is %d", sum(my_numbs, i));
 }
