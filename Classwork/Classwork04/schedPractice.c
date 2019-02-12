c1793d1e0fcea4e0f78dd34d2bbc42d13556bf79
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2019-02-11 22:19:55 -0800

    code to parse inputs

--- Classwork/Classwork04/schedPractice.c
+++ Classwork/Classwork04/schedPractice.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]) { return 0; }
+#define t_slice = 40;
+
+struct Thread {
+  char* name;
+  int run_time;
+};
+
+int main(int argc, char* argv[]) {
+
+  struct Thread threads[(argc - 1) / 2];
+
+  for (int i = 1; i < argc; i += 2) {
+    struct Thread test = {argv[i], atoi(argv[i + 1])};
+    threads[(i - 1) / 2] = test;
+  }
+
+  return 0;
+}
