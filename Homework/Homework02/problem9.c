60a5cc124fcaa6f484746ea824315a4d034c99d8
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2019-03-05 17:20:28 -0800

    first 2 problems

---
+++ Homework/Homework02/problem9.c
@@ -0,0 +1,8 @@
+#include <unistd.h>
+int main(int argc, char **argv) {
+  int i;
+  for (i = 0; i < 3; i++) { /* loops 3 times */
+    fork();                 /* each time calling fork */
+  }
+  sleep(30); /* then sleeps 30 seconds */
+}
