8bfef14525ad491b45cfc5c58e73a2a68f3871b5
Kevin Peters <35283725+kpeter51@users.noreply.github.com>
2019-03-07 13:06:18 -0800

    problem 8 (?)

--- Homework/Homework02/problem8.c
+++ Homework/Homework02/problem8.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 
 int main() {
   clock_t start_t, end_t, total_t;
 
-  char big_array[1000000];
+  clock_t totalstart_t = clock();
 
-  for (int i = 0; i < 1000000; i += 4096) {
+  char *big_array = malloc(sizeof(char) * 10000000);
+  for (int i = 0; i < 10000000; i++) {
+    big_array[i] = i;
+  }
+
+  for (int i = 0; i < 2000000; i += 4096) {
     start_t = clock();
     printf("%c", big_array[i]);
     end_t = clock();
     total_t = (double)(end_t - start_t) / CLOCKS_PER_SEC;
     printf("Total time taken by CPU: %f\n", total_t);
   }
+
+  clock_t totalend_t = clock();
+  clock_t totaltotal_t = (double)(totalend_t - totalstart_t) / CLOCKS_PER_SEC;
+  printf("Total time taken by CPU: %f\n", totaltotal_t);
 
   return (0);
 }
