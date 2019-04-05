a94b1fac3067a077b5d8db446cbcf95f277ad16f
JustusWest <38738198+JustusWest@users.noreply.github.com>
2019-04-05 16:16:41 -0700

    Update problem3.c

--- Homework/Homework03/problem3.c
+++ Homework/Homework03/problem3.c
@@ -1,34 +1,18 @@
 #include <fcntl.h>
 #include <stdio.h>
-#include <stdio.h>
 #include <sys/stat.h>
+#include <ctype.h>
+#include <unistd.h>
 #include <pthread.h>
 int main() {
-    pid_t returnedValue = fork();
-    if (returnedValue < 0) {
-        perror("error forking");
-        return -1;
-    } else if (returnedValue == 0) {
-        if (close(STDOUT_FILENO) < 0) {
-            perror("error closing standard output");
-            return -1;
-        }
-        // When there is no error, open returns the smallest file
-        // descriptor not already in use by this process, so having
-        // closed STDOUT_FILENO, the open should reuse that number.
-        if (open("my-processes", O_WRONLY | O_CREAT | O_TRUNC,
-                 S_IRUSR | S_IWUSR) < 0) {
-            perror("error opening my-processes");
-            return -1;
-        }
-        execlp("ps", "ps", "l", NULL); // ps with option letter l
-        perror("error executing ps");
-        return -1;
-    } else {
-        if (waitpid(returnedValue, 0, 0) < 0) {
-            perror("error waiting for child");
-            return -1;
-        }
-        cout << "Note the parent still has the old standard output." << endl;
+    FILE *input = fopen("./beeMovie.txt", "r");
+    FILE *output = fopen("./beeMovieUpper.txt", "w");
+    int currentChar;
+    currentChar = fgetc(input);
+    while(EOF != currentChar){
+      fputc(toupper(currentChar), output);
+      currentChar = fgetc(input);
     }
+    fclose("../Homework03/beeMovie.txt");
+    fclose("../Homework03/beeMovieUpper.txt");
 }
