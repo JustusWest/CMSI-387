f6edc31f906f7c65505fa2ca1d7b241e702ff2a4
Kevin Peters <35283725+kevinusername@users.noreply.github.com>
2019-04-05 22:01:27 -0700

    fixed problem 4

--- Homework/Homework03/problem4.c
+++ Homework/Homework03/problem4.c
@@ -4,13 +4,23 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <time.h>
+
+void printTime(clock_t t) {
+    t = clock() - t;
+    double time_taken = ((double)t) / CLOCKS_PER_SEC;
+    printf("Finished in %f seconds\n", time_taken);
+}
 
 int main(int argc, char *argv[]) {
+    clock_t t;
     int fd;
     struct stat s;
+    int status;
     size_t size;
     const char *mapped;
     int i;
+    t = clock();
     if (!argv[1]) {
         printf("Must provide file name to search for x's\n");
         return 2;
@@ -19,17 +29,20 @@ int main(int argc, char *argv[]) {
 
     fd = open(fname, O_RDONLY);
 
+    status = fstat(fd, &s);
     size = s.st_size;
 
     mapped = mmap(0, size, PROT_READ, MAP_PRIVATE, fd, 0);
 
     for (i = 0; i < size; i++) {
         char c = mapped[i];
-        if (c == 'x') {
+        if (c == 'x' || c == 'X') {
             printf("Found x in %s at character %d\n", fname, i);
+            printTime(t);
             return 0;
         }
     }
     printf("No x found in file %s\n", fname);
+    printTime(t);
     return 1;
 }
--- Homework/Homework03/writeup.md
+++ Homework/Homework03/writeup.md
@@ -4,6 +4,16 @@
 
 Our program avoids deadlock using the "resource hierarchy solution". The idea behind this is that each philosopher picks up and *locks* the lower numbered fork first, then the higher numbered fork. By using this system, the last philosopher will always be able to pick up both forks. Once he sets down the fork, the first philosopher will then be able to pick up both forks, this pattern then circles around the table so that every philosopher is able to eat.
 
+## Problem 4
+
+| file                      | size (bytes) | time (seconds) |
+| ------------------------- | ------------ | -------------- |
+| noX.txt                   | 0            | 0.000172       |
+| x.txt                     | 1            | 0.000188       |
+| beeMovie.txt              | 55,317       | 0.000200       |
+| extremelyLongWithX.txt    | 15,125,684   | 0.029887       |
+| extremelyLongWithOutX.txt | 15,125,682   | 0.029818       |
+
 ## Problem 5
 
 The bounded buffer class would be good to use.
